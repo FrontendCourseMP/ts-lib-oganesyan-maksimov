7568f63e9cd4f1efca5e1be686c50e52ac4c230c
stl2802 <anmaks93@gmail.com>
2025-11-26 13:17:10 +0300

    Максимов Андрей

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,12 @@
 export type sum = (a: number, b: number) => number
 
 export type User =  {
+    id:Int16Array
     fullname:string
     nicname:string
     age:string
     role:Role
+    tag:Tag
     email:"/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
 }
 
@@ -13,21 +15,30 @@ type Role = {
     prev:"admin"|"user"|"viewer"|"superadmin"
 }
 
-export type Time = {
+export type Data = {
     hour:Int16Array
     minuta:Int16Array
     second:Int16Array
 }
 
 export type video = {
+    id:Int16Array
     file:File
     name:string
-    tags:tag[]
+    tags:Tag[]
     status:boolean
     published:boolean
     payment:boolean
 }
 
-type tag = {
+type Tag = {
     name: string
+}
+
+export type Comment = {
+    id:Int16Array
+    value:string
+    user:User
+    createAt:Data
+    video:video
 }
